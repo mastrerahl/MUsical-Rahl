5ae1d6bb0d02bbc7d01536e86c8ee65bf7e1aaad
mastrerahl <lordrahl254@gmail.com>
2025-07-16 12:33:59 +0300

    Create search.js

---
+++ api/search.js
@@ -0,0 +1,18 @@
+// api/search.js
+const ytSearch = require('yt-search');
+
+module.exports = async (req, res) => {
+  const query = req.query.q;
+  if (!query) return res.status(400).json({ error: 'Missing search query' });
+
+  const results = await ytSearch(query);
+  const videos = results.videos.slice(0, 5).map(video => ({
+    title: video.title,
+    videoId: video.videoId,
+    thumbnail: video.thumbnail,
+    duration: video.timestamp,
+    download: `https://api.vevioz.com/api/button/mp3/${video.videoId}`
+  }));
+
+  res.json({ results: videos });
+};
